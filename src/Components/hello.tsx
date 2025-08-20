904552aa0680e6991dfa620c8390688ff289722c
Frank <Frankuik@hotmail.com>
2025-08-20 11:56:06 +0200

    develop ska inte h

--- src/Components/hello.tsx
+++ src/Components/hello.tsx
@@ -2,6 +2,9 @@ function divRunner() {
   return (
     <div>
       <text>Hello Sir</text>
+      <text>Hello Sir</text>
+      <text>Hello Sir</text>
+      <text>Hello Sir</text>
     </div>
   );
 }
